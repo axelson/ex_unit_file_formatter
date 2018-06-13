e1076eca2d1d1396fb3151aaf6834c08e6b28167
Jason Axelson <jason@hobnob.io>
2018-06-12 19:50:20 -1000

    Initial version of the file formatter with one simple test

--- lib/ex_unit_file_formatter.ex
+++ lib/ex_unit_file_formatter.ex
@@ -1,18 +1,59 @@
 defmodule ExUnitFileFormatter do
-  @moduledoc """
-  Documentation for ExUnitFileFormatter.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  @impl true
+  def init(_) do
+    {:ok, %{failed_files: %{}}}
+  end
 
-  ## Examples
+  @impl true
+  def handle_cast({:suite_started, _opts}, state) do
+    {:noreply, state}
+  end
 
-      iex> ExUnitFileFormatter.hello
-      :world
+  @impl true
+  def handle_cast({:suite_finished, _run_us, _load_us}, state) do
+    print_failed_files(state)
+    {:noreply, state}
+  end
 
-  """
-  def hello do
-    :world
+  @impl true
+  def handle_cast({:test_finished, %{state: nil}}, state) do
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast({:test_finished, %{state: {:failed, _}} = test}, state) do
+    file = test.tags.file
+
+    state = record_file_failed(state, file)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast(_event, state) do
+    {:noreply, state}
+  end
+
+  defp record_file_failed(%{failed_files: failed_files_map} = state, file) do
+    failed_files_map = Map.update(failed_files_map, file, 1, & &1 + 1)
+    Map.put(state, :failed_files, failed_files_map)
+  end
+
+  defp print_failed_files(%{failed_files: failed_files_map}) do
+    failed_file_list =
+      failed_files_map
+      |> Map.to_list()
+      |> Enum.sort(fn {_, count1}, {_, count2} ->
+        count1 >= count2
+      end)
+
+    if length(failed_file_list) > 0 do
+      IO.puts "\nFailed Files:"
+      failed_file_list
+      |> Enum.each(fn {file, count} ->
+        IO.puts("#{count}: #{file}")
+      end)
+    end
   end
 end
--- test/ex_unit_file_formatter_test.exs
+++ test/ex_unit_file_formatter_test.exs
@@ -2,7 +2,21 @@ defmodule ExUnitFileFormatterTest do
   use ExUnit.Case
   doctest ExUnitFileFormatter
 
-  test "greets the world" do
-    assert ExUnitFileFormatter.hello() == :world
+  @failed_test %{
+    state: {:failed, nil},
+    tags: %{file: "a_file"}
+  }
+
+  test "smoke test" do
+    output = ExUnit.CaptureIO.capture_io(fn ->
+      {:ok, pid} = GenServer.start_link(ExUnitFileFormatter, [])
+      GenServer.cast(pid, {:suite_started, nil})
+      GenServer.cast(pid, {:test_finished, @failed_test})
+      GenServer.cast(pid, {:suite_finished, nil, nil})
+      # Wait for the GenServer to process it's messages
+      _state = :sys.get_state(pid)
+    end)
+    IO.inspect(output, label: "output")
+    assert output == "\nFailed Files:\n1: a_file\n"
   end
 end
